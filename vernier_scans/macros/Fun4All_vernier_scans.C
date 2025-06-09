05edebaced2d13e4fc923854c3667964c7c6f42e
Dylan M Neff <dneffsph@sphnxuser01.sdcc.bnl.gov>
2025-06-08 20:24:41 -0400

    Added vernier_scans as a copy of ZDC_SMD

---
+++ vernier_scans/macros/Fun4All_vernier_scans.C
@@ -0,0 +1,98 @@
+#ifndef FUN4ALL_ZDC_SMD_C
+#define FUN4ALL_ZDC_SMD_C
+
+#include <zdc_smd/ZDC_SMD.h>
+
+//  -----------------------
+
+#include <G4_Input.C>
+#include <Calo_Calib.C>
+
+#include <caloreco/CaloTowerBuilder.h>
+
+#include <caloreco/CaloTowerCalib.h>
+#include <caloreco/CaloTowerStatus.h>
+#include <zdcinfo/ZdcReco.h>
+
+//  -----------------------
+
+#include <fun4allraw/Fun4AllPrdfInputManager.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllRunNodeInputManager.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllUtils.h>
+#include <fun4all/SubsysReco.h>
+
+#include <phool/recoConsts.h>
+
+#include <ffamodules/CDBInterface.h>
+#include <mbd/MbdReco.h>
+
+#include <iostream>
+#include <string>
+#include <cstdio> 
+
+// cppcheck-suppress unknownMacro
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libfun4allraw.so)
+R__LOAD_LIBRARY(libZDC_SMD.so)
+R__LOAD_LIBRARY(libmbd.so)
+
+//  -----------------------
+R__LOAD_LIBRARY(libzdcinfo.so)
+R__LOAD_LIBRARY(libcalo_reco.so)
+//  -----------------------
+
+void Fun4All_ZDC_SMD(
+    const std::string &fname = "DST_TRIGGERED_EVENT_run2pp_new_2024p001-00042797-0000.root",
+    const std::string &outname = "zdc_smd_42797_0000.root",
+    int startEvent = 0,
+    int nEvents = 10000,
+    const std::string &dbtag = "2024p007")
+{
+
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(0);
+
+  Fun4AllInputManager *in1 = new Fun4AllDstInputManager("in1");
+  in1->AddFile(fname);
+  se->registerInputManager(in1);
+
+  recoConsts *rc = recoConsts::instance();
+
+  rc->set_StringFlag("CDB_GLOBALTAG", dbtag);
+  CDBInterface::instance()->Verbosity(1);
+
+  // MBD/BBC Reconstruction
+  MbdReco *mbdreco = new MbdReco();
+  se->registerSubsystem(mbdreco);
+  
+  ZDC_SMD *zdc_smd = new ZDC_SMD();
+  zdc_smd->setFileName(outname);
+//  zdc_smd->setGainMatch("/sphenix/user/dloomis/analysis/ZDCNeutronLocPol/SMDrelativegains.dat");
+  se->registerSubsystem(zdc_smd);
+
+  if (startEvent > 0)
+  {
+    se->fileopen("in1",fname);
+    se->skip(startEvent);
+  }
+
+  se->run(nEvents);  
+  se->End();
+  se->PrintTimer();
+  se->Print();
+  std::cout << "Ran " << se->EventCounter() << " events" << std::endl;
+  delete se;
+  CDBInterface::instance()->Print();  // print used DB files
+  std::cout << "All done!" << std::endl;
+  gSystem->Exit(0);
+
+
+
+
+}
+
+#endif
